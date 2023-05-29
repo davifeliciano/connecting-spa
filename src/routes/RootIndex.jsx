c6e7605565d0bf75d2cdf4350378dec888a30328
Davi Feliciano <dfeliciano37@gmail.com>
2023-05-29 15:35:48 -0300

    feat: redirection from index route

--- src/routes/RootIndex.jsx
+++ src/routes/RootIndex.jsx
@@ -1,3 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import useAuth from "../hooks/useAuth.js";
+
 export default function RootIndex() {
+  const { auth } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (Object.keys(auth).length !== 0) return navigate("/feed");
+    navigate("/login");
+  }, [auth]);
+
   return <></>;
 }
